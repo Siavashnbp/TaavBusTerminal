55c608f25ef7d329f4057daefbfb01e2713ef202
Siavash Nabipour <51988240+Siavashnbp@users.noreply.github.com>
2024-01-24 21:32:29 +0330

    BusEntity

---
+++ Entities/Bus.cs
@@ -0,0 +1,21 @@
+﻿namespace BusTerminal.Entities
+{
+    public abstract class Bus
+    {
+        public Bus(string name, BusType busType)
+        {
+            Name = name;
+            BusType = busType;
+            SeatCount = busType == BusType.VIP ? 30 : 44;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int SeatCount { get; set; }
+        public BusType BusType { get; set; }
+    }
+    public enum BusType
+    {
+        Normal,
+        VIP
+    }
+}
--- obj/Debug/net8.0/BusTerminal.AssemblyInfo.cs
+++ obj/Debug/net8.0/BusTerminal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusTerminal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86baab98c2cd609aa5398d1054921d5c4fd0189b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusTerminal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusTerminal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
