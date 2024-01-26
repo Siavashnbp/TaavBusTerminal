77a29bcc989bb6207309a9043afa75cdef1a49d9
Siavash Nabipour <51988240+Siavashnbp@users.noreply.github.com>
2024-01-26 11:51:44 +0330

    Create database and entites
    1- first migration
    2- entities and relations

---
+++ EnitityMap/LocationEntityMap.cs
@@ -0,0 +1,18 @@
+﻿using BusTerminal.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BusTerminal.EnitityMap
+{
+    public class LocationEntityMap : IEntityTypeConfiguration<Location>
+    {
+        public void Configure(EntityTypeBuilder<Location> builder)
+        {
+            builder.HasKey(_ => _.Id);
+            builder.Property(_ => _.Id).UseIdentityColumn();
+            builder.Property(_ => _.Province).HasMaxLength(50);
+            builder.Property(_ => _.City).HasMaxLength(50);
+            builder.Property(_ => _.Name).HasMaxLength(50);
+        }
+    }
+}
---
+++ Entities/Trip.cs
@@ -0,0 +1,31 @@
+﻿namespace BusTerminal.Entities
+{
+    public class Trip
+    {
+        public Trip()
+        {
+
+        }
+        public Trip(Location origin, Location destination, Bus bus)
+        {
+            OriginId = origin.Id;
+            Origin = origin;
+            DestinationId = destination.Id;
+            Destination = destination;
+            BusId = bus.Id;
+            Bus = bus;
+        }
+
+        public int Id { get; set; }
+        public int OriginId { get; set; }
+        public Location Origin { get; set; }
+        public int DestinationId { get; set; }
+        public Location Destination { get; set; }
+        public int BusId { get; set; }
+        public Bus Bus { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public decimal SeatPrice { get; set; }
+        public int ReserveCancelation { get; set; }
+        public int PrchaseCancelation { get; set; }
+    }
+}
